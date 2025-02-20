74eba6bdce6a8d9188a22ff8a3cb49ae6535ab41
giovani souza <giovani.souza@insidecode.io>
2025-02-20 13:43:37 -0300

    feat(layout): create layout and page components
    - Added RootLayout component in layout.tsx to define the overall structure of the application.
    - Implemented metadata for the application, including title and description.
    - Created Home component in page.tsx to serve as the main entry point for the application.
    - Integrated header and various sections (Hero, About, Technologies, PortfolioTimeline, Contact) into the Home component.
    - Added scroll event handling to update the active section in the header based on the user's scroll position.
    
    These changes establish a foundational layout and page structure for the application, enhancing the user experience by providing a cohesive and interactive interface. The integration of scroll handling improves navigation, making it easier for users to engage with different sections of the portfolio.

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,57 @@
+import type React from "react";
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { TranslationsProvider } from "@/provider/TranslationsProvider";
+import initTranslations from "../i18n";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Developer Portfolio",
+  description: "Personal portfolio showcasing my work as a Software Engineer",
+  generator: "v0.dev",
+};
+
+export default async function RootLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  const i18nNamespaces = ["default"];
+  const { resources } = await initTranslations(locale, i18nNamespaces);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+        </head>
+        <body
+          className={`${inter.className}  bg-fixed  bg-gradient-to-t dark:from-primary/10  from-primary/15  to-background rounded-2xl p-4`}
+        >
+          <TranslationsProvider
+            namespaces={i18nNamespaces}
+            locale={locale}
+            resources={resources}
+          >
+            {children}
+          </TranslationsProvider>
+        </body>
+      </ThemeProvider>
+    </html>
+  );
+}
---
+++ app/[locale]/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Header from "@/components/header";
+import Hero from "@/components/sections/hero";
+import About from "@/components/sections/about";
+import Technologies from "@/components/sections/technologies";
+import Experience from "@/components/sections/experience";
+import Contact from "@/components/sections/contact";
+import PortfolioTimeline from "@/components/timeLine";
+
+export default function Home() {
+  const [activeSection, setActiveSection] = useState("home");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll("section");
+      const scrollPosition = window.scrollY + window.innerHeight / 2;
+
+      sections.forEach((section) => {
+        const top = section.offsetTop;
+        const height = section.offsetHeight;
+
+        if (scrollPosition >= top && scrollPosition < top + height) {
+          setActiveSection(section.id);
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="min-h-screen text-white">
+      <Header activeSection={activeSection} />
+      <main>
+        <Hero />
+        <About />
+        <Technologies />
+        {/* <Experience /> */}
+        <PortfolioTimeline />
+        <Contact />
+      </main>
+    </div>
+  );
+}
