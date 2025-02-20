4878bfc76f9782a9969e8ff980ac71935f89bd75
giovani souza <giovani.souza@insidecode.io>
2025-02-20 13:44:06 -0300

    feat(i18n): add internationalization configuration and middleware
    - Created a new file `i18nConfig.ts` to define internationalization settings.
    - Added `locales` and set the default locale to "pt".
    - Introduced `middleware.ts` to handle routing based on the internationalization configuration.
    - Implemented the `middleware` function to apply the i18n router to incoming requests.
    - Configured the middleware to match specific routes, excluding API and static files.
    
    These changes establish a foundational internationalization setup for the project, enabling support for multiple languages and improving user experience for Portuguese-speaking users.

---
+++ i18nConfig.ts
@@ -0,0 +1,9 @@
+import { Config } from "next-i18n-router/dist/types";
+import { locales } from "./mocks";
+
+const i18nConfig: Config = {
+  locales,
+  defaultLocale: "pt",
+};
+
+export default i18nConfig;
