3b8b5c157a40db8b5de35ebbd028e2d99120822d
giovani souza <giovani.souza@insidecode.io>
2025-03-08 09:05:52 -0300

    feat(modeToggle): update Sun icon styling for better visibility - Changed the class of the Sun icon in the ModeToggle component to include 'text-foreground' for improved visibility. - Ensured the icon maintains its transition effects and responsive design.
    This update enhances the visual clarity of the Sun icon in the mode toggle, contributing to a better user experience in the interface.

--- src/components/modeToggle.tsx
+++ src/components/modeToggle.tsx
@@ -24,7 +24,7 @@ function ModeToggle({ showText = false }: ModeToggleProps) {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon" className="px-4 w-auto  gap-2 ">
           {!showText && (
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Sun className="h-[1.2rem] w-[1.2rem] text-foreground rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           )}
           {!showText && (
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
